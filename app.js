63c32c1dc3d00d3ac8ec8700dd95e8bf8ba3cc12
Joominseok <jumseok12@gmail.com>
2022-12-30 19:59:21 +0900

    확인 안했으면 모를뻔... 너무 급했나바 나

--- app.js
+++ app.js
@@ -8,4 +8,18 @@ app.get("/querystring", (req,res) => {
   res.send("querystring page")
 })
 
+
+app.post("/body-formdata", (req,res) => {
+  res.send("body-formdata page")
+})
+
+app.post("/body-mulipart", (req,res) => {
+  res.send("body-mulipart page")
+})
+
+app.post("/body-json", (req,res) => {
+  res.send("body-json page")
+})
+
+
 app.listen(3000,() => console.log(3000,"번 퐅으로 섭어 열림"))
